37e4aca9d058ee7de6196d9e842d20545ce83efe
Usama Subhani <reachusama@protonmail.com>
2020-07-31 22:20:44 +0500

    Update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Usama Subhani`,
+    title: `Based Portfolio`,
     description: `WOWOW`,
     author: `@basedusama`,
   },
