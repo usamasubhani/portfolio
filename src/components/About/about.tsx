e36ad4580c366e593dd7d2a6470689bf63431c68
Usama Subhani <reachusama@protonmail.com>
2020-07-31 23:51:31 +0500

    Add content to "About" component

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 import './about.css'
 const About = () => {
     return (
-        <div className="about">
-            
+        <div className="about-container">
+            <div className="about-description-container about-contnent">
+                <p>This is my new portfolio. It is built using TypeScript, Gatsby and Contentful</p>
+                <p>I am a Full Stack Software Developer.</p>
+                <p>I help companies and individuals in optimizing workflows, automating processes, developing new products and updating/fixing existing codebases.</p>
+            </div>
+            <div className="about-interests-container about-contnent">
+                <p>I am interested in: </p>
+                <p>>  Artificial Intelligence</p>
+                <p>>  Open Source</p>
+                <p>>  Digital Minimalism</p>
+            </div>
         </div>
     )
 }
