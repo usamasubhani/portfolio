74d0b93f92683179de6521c6e463630ca99dbe83
Usama Subhani <reachusama@protonmail.com>
2020-07-31 20:15:17 +0500

    Add site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby TypeScript + Contentful Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Usama Subhani`,
+    description: `WOWOW`,
+    author: `@basedusama`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
