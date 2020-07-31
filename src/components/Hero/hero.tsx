01e799f3865cb0f49a666e9215b0e2b901966d2d
Usama Subhani <reachusama@protonmail.com>
2020-07-31 23:34:07 +0500

    Add title and social links

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -4,7 +4,16 @@ import './hero.css'
 const Hero = () => {
     return (
         <div className="hero">
-            
+            <div className="hero-title-container">
+                <h1>
+                    Hello, <br/> I am <br/> Usama Subhani
+                </h1>
+            </div>
+            <div className="social-icons-container">
+            <a href="https://twitter.com/basedusama" target="_blank">twitter</a>
+            <a href="https://github.com/usamasubhani" target="_blank">github</a>
+            <a href="https://www.linkedin.com/in/usamasubhani/" target="_blank">linkedin</a>
+            </div>
         </div>
     )
 }
