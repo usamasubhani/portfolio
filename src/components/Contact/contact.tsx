c9dd04f65d269ef65a0abc356d9450eb31bed595
Usama Subhani <reachusama@protonmail.com>
2020-08-01 19:06:35 +0500

    Add contact section

--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -3,8 +3,12 @@ import './contact.css'
 
 const Contact = () => {
     return (
-        <div className="contact">
-            
+        <div className="contact-section">
+            <div className="contact-text" >
+                <h1>I'M ALWAYS LOOKING FOR NEW AND CREATIVE PROJECTS.</h1>
+                <h1 className="red">Let's Talk</h1>
+                <a className="red" href="mailto:reachusama@pm.me">reachusama@pm.me</a>
+            </div>
         </div>
     )
 }
