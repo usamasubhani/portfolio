9a6421f333b7ac20472c8e9ea875afcf8dc881dd
Usama Subhani <reachusama@protonmail.com>
2020-08-03 16:34:52 +0500

    Make punctuations red

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -6,11 +6,12 @@ const About = () => {
     return (
         <div className="about-container">
             <div className="about-description-container about-contnent">
-                <p>I am a Full Stack Developer.</p>
-                <p>I can develop effective and efficient software products, using best practices and cutting edge technologies. I am driven by the goal of improving human's lives through my knowledge and skills.</p>
+                <p>I am a Full Stack Developer<span className="red">.</span></p>
+                <p>I can develop effective and efficient software products<span className="red">,</span> using cutting edge technologies and best practices<span className="red">.</span></p>
+                <p>Driven by the goal of improving human's lives through my knowledge and skills<span className="red">,</span> I am committed to life long learning and hard work to build a better future<span className="red">.</span></p>
             </div>
             <div className="about-interests-container about-contnent">
-                <p>I am interested in: </p>
+                <p>I am interested in<span className="red">:</span> </p>
                 <p><FontAwesomeIcon icon={faArrowRight}/>  Artificial Intelligence</p>
                 <p><FontAwesomeIcon icon={faArrowRight}/>  Open Source</p>
                 <p><FontAwesomeIcon icon={faArrowRight}/>  Digital Minimalism</p>
--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
         <div className="contact-section">
             <div className="contact-text" >
-                <h1>I'M ALWAYS LOOKING FOR CHALLENGING PROJECTS.</h1>
+                <h1>I<span className="red">'</span>M ALWAYS LOOKING FOR CHALLENGING PROJECTS<span className="red">.</span></h1>
                 <h1 >Let<span className="red">'</span>s Talk</h1>
                 <a className="red" href="mailto:reachusama@pm.me">reachusama@pm.me</a>
             </div>
