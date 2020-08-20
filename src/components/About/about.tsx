df559f6f4f76d1605108cc6440bc1f4dcdf8483c
Usama Subhani <reachusama@protonmail.com>
2020-08-20 19:33:02 +0500

    Update about description

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -8,7 +8,7 @@ const About = () => {
             <div className="about-description-container about-contnent">
                 <p>I am a Full Stack Developer<span className="red">.</span></p>
                 <p>I can develop effective and efficient software products<span className="red">,</span> using cutting edge technologies and best practices<span className="red">.</span></p>
-                <p>Driven by the goal of improving human's lives through my knowledge and skills<span className="red">,</span> I am committed to life long learning and hard work to build a better future<span className="red">.</span></p>
+                <p>Driven by the goal of improving human's lives through technology<span className="red">,</span> I am committed to life long learning and hard work to build a better future<span className="red">.</span></p>
             </div>
             <div className="about-interests-container about-contnent">
                 <p>I am interested in<span className="red">:</span> </p>
